892fce0b3058c4849ce17c94b0d4b03fe9865a34
yc97463 <hi@imych.one>
2025-03-28 11:19:41 +0800

    add problem 2 figure in diff

--- 20250328/make_diff/demo_diff_tanh.m
+++ 20250328/make_diff/demo_diff_tanh.m
@@ -1,20 +1,52 @@
 function demo_diff_tanh()
 x = sym('x');
 ss = 'tanh(x)';
-fx = inline(ss);
+% fx = inline(ss);
+f = inline(ss);
 
 inst = "diff("+ss+")";
 ss1 = eval(inst);
-fx1 = inline(ss1) % 一階導數算出來了
+f1 = inline(ss1) % 一階導數算出來了
 
 inst2 = "diff("+string(ss1)+")";
 ss2 = eval(inst2);
-fx2 = inline(ss2) % 二階導數
+f2 = inline(ss2) % 二階導數
+
+inst3 = "diff("+string(ss2)+")";
+ss3 = eval(inst3);
+f3 = inline(ss3) % 三階導數
 
 % graph
 
 a = linspace(-2*pi, 2*pi);
-plot(a, fx(a));
-hold on;
-plot(a, fx1(a), 'r');
-plot(a, fx2(a), 'k');
+
+% problem 1
+plot(a, f(a)); hold on;
+plot(a, f1(a), 'r');
+plot(a, f2(a), 'k'); title("problem 1");
+figure;
+
+% problem 2
+
+n = length(a);
+c = 0;
+for i = 1:n
+    y(i) = f(c);
+    y1(i) = f1(c)*(a(i)-c);
+    y2(i) = f2(c)/2*(a(i)-c)^2;
+    y3(i) = f3(c)/6*(a(i)-c)^3;
+end
+plot(a, f(a)); hold on;
+plot(a, y+y1, 'r'); title("two terms");
+figure;
+
+plot(a, f(a)); hold on;
+plot(a, y+y1+y2, 'b'); title("three terms");
+figure;
+
+plot(a, f(a)); hold on;
+plot(a, y+y1+y2+y3, 'g'); title("four terms");
+figure;
+
+
+
